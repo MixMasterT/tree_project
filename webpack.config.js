332177d29e7c2cefd15d80eb35e99f3269b9d239
mixmastert <torahlight@gmail.com>
2017-05-12 22:14:38 -0700

    initial commit build basic setup and add es-lint

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./app.js",
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: "bundle.js",
+    publicPath: path.join(__dirname, 'build'),
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      },
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: [".js", ".jsx" ]
+  }
+};
